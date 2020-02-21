ea4d9821f7ec626772d2201ebde4ec2ab2ec1486
meteatbs <43855185+meteatbs@users.noreply.github.com>
2020-02-22 01:19:15 +0300

    Add files via upload

---
+++ EntityFrameworkGridApp/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace EntityFrameworkGridApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtisim = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.dgvMusteri = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSehir = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btniptal = new System.Windows.Forms.Button();
+            this.CustomerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.City = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMusteri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim";
+            // 
+            // txtisim
+            // 
+            this.txtisim.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtisim.Location = new System.Drawing.Point(284, 25);
+            this.txtisim.Name = "txtisim";
+            this.txtisim.Size = new System.Drawing.Size(100, 38);
+            this.txtisim.TabIndex = 1;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKaydet.Location = new System.Drawing.Point(138, 285);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(111, 48);
+            this.btnKaydet.TabIndex = 2;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // dgvMusteri
+            // 
+            this.dgvMusteri.AllowUserToDeleteRows = false;
+            this.dgvMusteri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMusteri.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CustomerID,
+            this.FirstName,
+            this.LastName,
+            this.City});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Lime;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvMusteri.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvMusteri.Location = new System.Drawing.Point(491, 25);
+            this.dgvMusteri.Name = "dgvMusteri";
+            this.dgvMusteri.ReadOnly = true;
+            this.dgvMusteri.RowTemplate.Height = 30;
+            this.dgvMusteri.Size = new System.Drawing.Size(305, 294);
+            this.dgvMusteri.TabIndex = 3;
+            this.dgvMusteri.DoubleClick += new System.EventHandler(this.dgvMusteri_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(109, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyadı";
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoyadi.Location = new System.Drawing.Point(284, 72);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(100, 38);
+            this.txtSoyadi.TabIndex = 1;
+            this.txtSoyadi.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(109, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Şehir";
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSehir.Location = new System.Drawing.Point(284, 123);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(100, 38);
+            this.txtSehir.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 31);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Adres";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdres.Location = new System.Drawing.Point(270, 173);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(154, 55);
+            this.txtAdres.TabIndex = 1;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSil.Location = new System.Drawing.Point(270, 285);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(89, 48);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btniptal
+            // 
+            this.btniptal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btniptal.Location = new System.Drawing.Point(379, 285);
+            this.btniptal.Name = "btniptal";
+            this.btniptal.Size = new System.Drawing.Size(92, 48);
+            this.btniptal.TabIndex = 2;
+            this.btniptal.Text = "İptal et";
+            this.btniptal.UseVisualStyleBackColor = true;
+            this.btniptal.Click += new System.EventHandler(this.btniptal_Click);
+            // 
+            // CustomerID
+            // 
+            this.CustomerID.DataPropertyName = "CustomerID";
+            this.CustomerID.HeaderText = "CustomerID";
+            this.CustomerID.Name = "CustomerID";
+            this.CustomerID.ReadOnly = true;
+            this.CustomerID.Visible = false;
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FirstName.DataPropertyName = "FirstName";
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // LastName
+            // 
+            this.LastName.DataPropertyName = "LastName";
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            // 
+            // City
+            // 
+            this.City.DataPropertyName = "City";
+            this.City.HeaderText = "City";
+            this.City.Name = "City";
+            this.City.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvMusteri);
+            this.Controls.Add(this.btniptal);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtisim);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Entity FrameWork Insert Update and Delete";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMusteri)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtisim;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.DataGridView dgvMusteri;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSehir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btniptal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustomerID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn City;
+    }
+}
+
---
+++ EntityFrameworkGridApp/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkGridApp
+{
+    public partial class Form1 : Form
+    {
+        Customer model = new Customer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btniptal_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+        void Clear()
+        {
+            txtisim.Text = txtSoyadi.Text = txtSehir.Text = txtAdres.Text = "";
+            btnKaydet.Text = "Save";
+            btniptal.Enabled = false;
+            model.CustomerID = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            PopulateDataGridView();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            model.FirstName = txtisim.Text.Trim();
+            model.LastName = txtSoyadi.Text.Trim();
+            model.City = txtSehir.Text.Trim();
+            model.Address = txtAdres.Text.Trim();
+            using (DBEntities db = new DBEntities())
+            {
+                if (model.CustomerID == 0)//insert
+                    db.Customers.Add(model);
+                else//update
+                    db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();                          
+            }
+            Clear();
+            PopulateDataGridView();
+            MessageBox.Show("Basariyla Kaydedildi");
+        }
+        void PopulateDataGridView()
+        {
+            dgvMusteri.AutoGenerateColumns = false;
+             using(DBEntities db=new DBEntities())
+            {
+                dgvMusteri.DataSource = db.Customers.ToList<Customer>();
+            }
+        }
+
+        private void dgvMusteri_DoubleClick(object sender, EventArgs e)
+        {
+            if(dgvMusteri.CurrentRow.Index != -1)
+            {
+                model.CustomerID = Convert.ToInt32(dgvMusteri.CurrentRow.Cells["CustomerID"].Value);
+                 using(DBEntities db=new DBEntities())
+                {
+                    model = db.Customers.Where(x => x.CustomerID == model.CustomerID).FirstOrDefault();
+                    txtisim.Text = model.FirstName;
+                    txtSoyadi.Text = model.LastName;
+                    txtSehir.Text = model.City;
+                    txtAdres.Text = model.Address;
+                }
+                btnKaydet.Text = "Guncellendi";
+                btnSil.Enabled = true;
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Bu kayıdı silmek istediğinizden emin misiniz?","EF Crud Operation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            using(DBEntities db=new DBEntities())
+            {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.Customers.Attach(model);
+                    db.Customers.Remove(model);
+                    db.SaveChanges();
+                    PopulateDataGridView();
+                    Clear();
+                    MessageBox.Show("Basariyla Silindi");
+            }
+        }
+    }
+}
